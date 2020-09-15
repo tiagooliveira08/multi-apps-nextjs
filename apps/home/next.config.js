77c0286d5fb98acaabaf6bc3dd0c761ba9fc7be9
Tiago <tiago_oliveira12@hotmail.com.br>
2020-09-15 16:25:17 -0300

    remove node_modules

--- apps/home/next.config.js
+++ apps/home/next.config.js
@@ -1,25 +1,8 @@
 const assetPrefix = process.env.BUILDING_FOR_NOW ? "/apps/home" : "";
 
 module.exports = {
-  transpileModules: ["./../components"],
   assetPrefix,
   env: {
     ASSET_PREFIX: assetPrefix,
-  },
-  webpack(config, { dev }) {
-    config.module.rules.forEach((rule) => {
-      console.log(rule);
-      const ruleContainsTs =
-        rule.test && rule.test.toString().includes("ts|tsx");
-
-      if (
-        ruleContainsTs &&
-        rule.use &&
-        rule.use.loader === "next-babel-loader"
-      ) {
-        rule.include = undefined;
-      }
-    });
-    return config;
   },
 };
